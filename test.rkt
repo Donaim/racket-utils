bc68318081ee0e9e1c91d90248d86128d14cd208
Donaim <3Donaim@gmail.com>
2019-12-03 20:08:29 +0100

    add test for parameterized letin-with

--- test.rkt
+++ test.rkt
@@ -6,14 +6,23 @@
 ((* _ 2) 2)
 
 (letin-with-full dom-print
-     [a (+ 2 7)]
-     [b (* a 10)]
-     (* b b))
+                 [a (+ 2 7)]
+                 [b (* a 10)]
+                 (* b b))
 
-(printf "dom-default = ~a\n"
-        (dom (dom-default (= _ 0))
-             [a (+ 2 7)]
-             [b (* a 10)]
-             [c (- b b)]
-             (+ 100 c)))
+(define [calc-default]
+  (dom (dom-default (= _ 0))
+       [a (+ 2 7)]
+       [b (* a 10)]
+       [c (- b b)]
+       (+ 100 c)))
+
+(printf "dom-default = ~a\n" (calc-default))
+
+(printf "dom parameterized = ~a\n"
+        (letin-parameterize
+         (lm f fname
+             (lm xqt resultqt x cont
+                 (add1 (f xqt resultqt x cont))))
+         (calc-default)))
 
